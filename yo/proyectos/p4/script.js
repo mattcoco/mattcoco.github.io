7a64af4e6e57cd613fdfa2ed84227782f57d5bfb
mattcoco <75008540+mattcoco@users.noreply.github.com>
2023-11-15 14:44:21 +0100

    Updates

--- yo/proyectos/p4/script.js
+++
@@ -1,9 +0,0 @@
-const search = document.querySelector('.search');
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-
-btn.addEventListener('click', () => {
-    search.classList.toggle('active');
-    // Pone el focus aqui, para eso hay un metodo simple
-    input.focus();
-})
